aa84a686332c6e92aa5b91985176a713dc91091e
Deepak Soni <md.deepaksoni@gmail.com>
2020-08-07 18:53:40 +0530

    Lambok integration for getters and setters

--- spring-data/spring-data-jpa/src/main/java/me/deepak/learning/spring/data/jpa/model/Employee.java
+++ spring-data/spring-data-jpa/src/main/java/me/deepak/learning/spring/data/jpa/model/Employee.java
@@ -1,11 +1,14 @@
 package me.deepak.learning.spring.data.jpa.model;
 
+import lombok.Data;
+
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.Id;
 import javax.persistence.Table;
 import java.time.LocalDate;
 
+@Data
 @Entity
 @Table(name = "TBL_EMPLOYEES")
 public class Employee {
@@ -34,42 +37,6 @@ public class Employee {
         this.lastName = builder.lastName;
         this.email = builder.email;
         this.joiningDate = builder.joiningDate;
-    }
-
-    public Long getId() {
-        return id;
-    }
-
-    public String getFirstName() {
-        return firstName;
-    }
-
-    public void setFirstName(String firstName) {
-        this.firstName = firstName;
-    }
-
-    public String getLastName() {
-        return lastName;
-    }
-
-    public void setLastName(String lastName) {
-        this.lastName = lastName;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    public LocalDate getJoiningDate() {
-        return joiningDate;
-    }
-
-    public void setJoiningDate(LocalDate joiningDate) {
-        this.joiningDate = joiningDate;
     }
 
     public static class Builder {
